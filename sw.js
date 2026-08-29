4c8e071729e986f2f1b5c6f17e4cb48bbf197bac
msajben2 <m.sajben2@gmail.com>
2026-08-29 21:40:14 +0200

    Add service worker for caching and offline support

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'organizator-v1';
+const urlsToCache = [
+    './',
+    './index.html',
+    './style.css',
+    './script.js'
+];
+
+// Inštalácia Service Workera a uloženie súborov do pamäte
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+        .then(cache => cache.addAll(urlsToCache))
+    );
+});
+
+// Získavanie súborov (aby appka fungovala aj offline)
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+        .then(response => response || fetch(event.request))
+    );
+});
